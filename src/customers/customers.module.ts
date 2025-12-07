9185bc21fa9dba0e9afec48d6716451a3dbda15f
apex17-8 <the2002apex@gmail.com>
2025-12-07 12:47:53 +0300

    azure8 stage backend commit

--- src/customers/customers.module.ts
+++ src/customers/customers.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CustomerService } from './customers.service';
 import { CustomersController } from './customers.controller';
 import { Customer } from './entities/customer.entity';
+import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Customer])],
+  imports: [TypeOrmModule.forFeature([Customer]), UsersModule],
   controllers: [CustomersController],
   providers: [CustomerService],
   exports: [CustomerService],
