a1378307360097f090d1354e8adb31f352e206c3
apex17-8 <the2002apex@gmail.com>
2025-12-10 08:03:02 +0300

    last azure database backhead commit

--- src/rider_locations/rider_locations.module.ts
+++ src/rider_locations/rider_locations.module.ts
@@ -6,12 +6,14 @@ import { Rider } from '../riders/entities/rider.entity';
 import { Restaurant } from '../restaurants/entities/restaurant.entity';
 import { RiderLocationService } from './rider_locations.service';
 import { RiderLocationController } from './rider_locations.controller';
-import { UsersModule } from '../users/users.module'; // Add this import
+import { UsersModule } from '../users/users.module';
+import { RiderRequestsModule } from '../rider-request/rider-request.module'; // Add this import
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([RiderLocation, Rider, Restaurant]),
-    UsersModule, // Add this line - this makes UserRepository available
+    UsersModule,
+    RiderRequestsModule, // Add this line
   ],
   providers: [RiderLocationService],
   controllers: [RiderLocationController],
